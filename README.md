d7a439f7519d2b5a4245e22a9ee7fe6462b6e9e6
Dom B <dominicbrown4@hotmail.com>
2023-11-27 16:33:55 +0000

    adding updated files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Weather-Dashboard
 A repo to hold the Server APIs Weather Dashboard challenge.
+
+
+
+# Resources - 
+
+Open Weather API Lat and Long - 
+
+https://openweathermap.org/api/geocoding-api
+
+https://stackoverflow.com/questions/65373299/how-can-i-use-city-name-instead-of-lat-and-log-in-openweather-api
+
+
--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 var cities = ["Berlin", "Paris", "Edinburgh", "Madrid", "Birmingham", "London"];
 
 var APIKey = "f4b03ff7f1a0faf5f4e8e64135541dab";
-var queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey;
 
-fetch(queryURL)
+function weatherData(city, lat, lon) {
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=f4b03ff7f1a0faf5f4e8e64135541dab`;
+  
+  fetch(queryURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
   var stringData = JSON.stringify(data);
     console.log(queryURL);
-    console.log(data);
-
+    console.log(stringData);
 });  
+}
 
-fetch(queryURL)
-.then(function (response) {
-  return response.json();
-}).then(function (data) {
-var stringData = JSON.stringify(data);
-$("#movie-view").text(stringData);
-});
+function getCoord(city) {
+  var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=f4b03ff7f1a0faf5f4e8e64135541dab`;
 
+  fetch(geoURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    if (data.length > 0) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      weatherData(city, lat, lon);
+    } else {
+      console.log("no data found for ${city}");
+    }
+  });
+}
 
-var movies = ["The Matrix", "Dune", "Mr. Right", "The Lion King"];
-
- // Function for displaying movie data
- function renderButtons() {
-
-  $("#buttons-view").empty();
-  // YOUR CODE GOES HERE
-  for (var i = 0; i < movies.length; i++) {
-    var btn = $("<button>");
-    btn.text(movies[i]);
-    btn.attr("data-movie", movies[i]);
-    $("#buttons-view").append(btn);
-  }
-
- }
+for (var i = 0; i < cities.length; i++) {
+  getCoord(cities[i]);
+}
